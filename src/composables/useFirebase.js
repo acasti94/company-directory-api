7d1beda28f62a676ccb2f8fd39979a380d891e97
acasti94 <108596779+acasti94@users.noreply.github.com>
2024-11-12 18:04:17 -0600

    Initial commit

---
+++ src/composables/useFirebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBORSJm91fRck5N85AiOBfqhQfZ22ioTlo",
+  authDomain: "homework5-16b33.firebaseapp.com",
+  projectId: "homework5-16b33",
+  storageBucket: "homework5-16b33.appspot.com",
+  messagingSenderId: "688585966724",
+  appId: "1:688585966724:web:03979b0616639e2d69eed1"
+};
+
+export const firebaseApp = initializeApp(firebaseConfig);
